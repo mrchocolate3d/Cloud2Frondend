fcb1bc42de0a240a5342110c4604d3e2d11afe5e
mrchocolate3d <43957746+mrchocolate3d@users.noreply.github.com>
2023-01-29 21:02:40 +0100

    get list of students

--- SchoolMessageSender/src/stores/message.js
+++ SchoolMessageSender/src/stores/message.js
@@ -1,22 +1,24 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const userMessageStore = defineStore('counter', 
 {
+  data() {
+    return {
+      results: [],
+    };
+  },
 actions: {
     async SendMultiple(values) {
         try {
-          console.log(values.SenderEmail)
-          console.log(values.SenderName)
-          console.log(values.ReceiverEmail)
-          console.log(values.ReceiverName)
-          console.log(values.Body)
-          console.log(values.Subject)
-          let Remail = values.ReceiverEmail.split(',')
-          console.log(Remail)
-          let Rname = values.ReceiverEmail.split(',')
+          console.log(values)
+
+          let Rname = values.ReceiverName.split(',')
           console.log(Rname)
 
+          let Remail = values.ReceiverEmail.split(',')
+          console.log(Remail)
             axios.post('http://localhost/SendMultiple', {
               SenderEmail: values.SenderEmail,
               SenderName: values.SenderName,
@@ -29,8 +31,6 @@ actions: {
             .then((res) => {
               console.log('This method is being passed')
               console.log(res)
-              router.push('/usersList');
-
             })
           } catch (error) {
             console.log(error);
@@ -38,15 +38,7 @@ actions: {
     },
     async Send(values){
         try {
-            console.log(values.SenderEmail)
-            console.log(values.SenderName)
-            console.log(values.ReceiverEmail)
-            console.log(values.ReceiverName)
-            console.log(values.Body)
-            console.log(values.Subject)
-            let arr = values.ReceiverEmail.split(',')
-            console.log(arr)
-
+            console.log(values)
             axios.post('http://localhost/Send', {
                 SenderEmail: values.SenderEmail,
                 SenderName: values.SenderName,
@@ -59,6 +51,48 @@ actions: {
             } catch (error) {
               console.log(error);
             }     
-    }
-  },
+    },
+    async GetListOfStudents(id){
+      return axios.get('http://127.0.0.1:5000/v1/classes/ ' + id + '/students')
+    },
+    async SendClassMsg(values,students){
+      console.log(values)
+      console.log(students)
+
+      students.forEach(element => {
+        values.ReceiverName += element.firstname+ ','
+      });
+
+      students.forEach(element => {
+        values.ReceiverEmail += element.email+ ','
+      });
+
+      let Rname = values.ReceiverName.split(',')
+      console.log(Rname)
+      Rname.pop()
+
+      let Remail = values.ReceiverEmail.split(',')
+      console.log(Remail)
+      Remail.pop()
+
+      try{
+        axios.post('http://localhost/SendMultiple', {
+          SenderEmail: values.SenderEmail,
+          SenderName: values.SenderName,
+          ReceiverEmail:Remail,
+          ReceiverName: Rname,
+          Body: values.Body,
+          Subject: values.Subject,
+
+        })
+        .then((res) => {
+          console.log('This method is being passed')
+          console.log(res)
+        })
+      } catch (error) {
+        console.log(error);
+      }
+      }
+    
+  }
 })
